00d7c99207d8ef5e8f4365de254ece0af1c1e787
KingA2009 <ajamoliddinov12@gmail.com>
2023-05-04 09:36:40 -0400

    check

---
+++ index.js
@@ -0,0 +1,30 @@
+// let a = +prompt("Assalomu aleykum Asadbekni kankulyatoriga xush kelibsiz \n 1-sonni kiriting")
+// let b = prompt("ishorani tanlang:\n 1 = + \n 2 = - \n 3 = * \n 4 = /")
+// let c = +prompt("2-sonni kiriting:")
+
+// if(b === "1"){
+//     alert(a + " + " + b + " = " + (a + b))
+// }
+
+// else if(b === "2"){
+//     alert(a + " - " + b + " = " + (a - b))
+// }
+
+// else if(b === "3"){
+//     alert(a + " * " + b + " = " + (a * b))
+// }
+
+// else if(b === "4"){
+//     alert(a + " / " + b + " = " + (a / b))
+// }
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Uy ishi
+
+a = +prompt("Son kiriting")
+
+if(a % 2 == 0){
+    alert("juft")
+}
+else{
+    alert("toq")
+}
